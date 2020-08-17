e68430e6d9da5e70878c14be5062f9f10f18c7c8
ashishsaboo <52492321+ashishsaboo@users.noreply.github.com>
2020-08-17 22:10:56 +0530

    added chromedriver for linux

--- autothon/com/source/driver/DriverInit.py
+++ autothon/com/source/driver/DriverInit.py
@@ -29,7 +29,8 @@ class WebDriver:
             options = webdriver.ChromeOptions()
             options.add_argument('headless')
             options.add_argument('window-size=1200x600')
-            self.driver = webdriver.Chrome(executable_path='resources/drivers/chromedriver', chrome_options=options)
+            self.driver = webdriver.Chrome(executable_path='resources/drivers/chromedriver-linux', chrome_options=options)
+            #self.driver = webdriver.Chrome(executable_path='resources/drivers/chromedriver', chrome_options=options)
             self.driver.maximize_window()
         else:
             print("using existing driver")
