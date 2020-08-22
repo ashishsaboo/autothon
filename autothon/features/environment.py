39bc84c6d91d125ead332b552e8c30c03fb2b40b
ashishsaboo <52492321+ashishsaboo@users.noreply.github.com>
2020-08-22 09:03:11 +0530

    added parallel run

--- autothon/features/environment.py
+++ autothon/features/environment.py
@@ -1,6 +1,6 @@
 from com.source.driver.DriverInit import WebDriver
 from behave import fixture
-   
+import os   
 def before_tag(context, tag):
     print("I am in before tag")
     webDriver=WebDriver()
@@ -10,4 +10,15 @@ def after_tag(context, tag):
     webDriver=WebDriver().getObject(WebDriver)
     webDriver.getDriver().quit()
     WebDriver().setObjectToNone()
+    deleteFileWithContent("skipped")
+
+def deleteFileWithContent(content):    
+    entries = os.listdir('output/allure-reports/')
+    for entry in entries:
+        print(entry)
+        file1 = open('output/allure-reports/' + entry, 'r')
+        if content in file1.read():
+            os.remove('output/allure-reports/' + entry)
+
+
     
