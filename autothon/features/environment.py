692a7411fe4cc7e7d07e5d7fab7c74f13de929ad
ashishsaboo <52492321+ashishsaboo@users.noreply.github.com>
2020-08-26 08:13:17 +0530

    Update environment.py

--- autothon/features/environment.py
+++ autothon/features/environment.py
@@ -4,15 +4,34 @@ import os
 def before_tag(context, tag):
     print("I am in before tag")
     webDriver=WebDriver()
+    curr_work_dir = os.getcwd()
+    print("directory creation for output : " + str(os.path.exists(curr_work_dir + '/output/')))
+    if not os.path.exists(curr_work_dir + '/output/') :
+        os.makedirs(curr_work_dir + '/output/')
+    print("directory creation for log: " + str(curr_work_dir + '/output/log/'))
+    if not os.path.exists(curr_work_dir + '/output/log/') :
+        os.makedirs(curr_work_dir + '/output/log/')
+    print("directory creation for screenshots: " + str(curr_work_dir + '/output/screenshots/'))
+    if not os.path.exists(curr_work_dir + '/output/screenshots/') :
+        os.makedirs(curr_work_dir + '/output/screenshots/')
     
     
 def after_tag(context, tag):
     print("I am in after tag")
     webDriver=WebDriver().getObject(WebDriver)
-    webDriver.getDriver().quit()
+    driver=webDriver.getDriver()
+    driver.quit()
     WebDriver().setObjectToNone()
     deleteFileWithContent("skipped")
-
+    
+def after_step(context, step):
+    if step.status == "failed":
+        print("I am in after step")
+        webDriver=WebDriver().getObject(WebDriver)
+        driver=webDriver.getDriver()
+        curr_work_dir = os.getcwd()
+        driver.get_screenshot_as_file(curr_work_dir + '/output/screenshots' + "/" + context.scenario.name + ".jpg")
+        
 def deleteFileWithContent(content):    
     entries = os.listdir('output/allure-reports/')
     for entry in entries:
