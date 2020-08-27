7041bef2177db2591b48ca6142d818955d934e17
gururaj-p010 <guru.patil010@gmail.com>
2020-08-27 10:41:13 +0530

    Updated start sh file

--- autothon/start.sh
+++ autothon/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 behave -f allure_behave.formatter:AllureFormatter -o ./output/allure-reports ./features --tags=@webservice
 
-allure serve ./output/allure-reports --port 8080
+allure serve ./output/allure-reports --port 9090
