042043761f13ed19012a72d50924b2043bdbb9b9
gururaj-p010 <guru.patil010@gmail.com>
2020-08-28 13:09:43 +0530

    Updated sh

--- autothon/start.sh
+++ autothon/start.sh
@@ -2,3 +2,12 @@
 behave -f allure_behave.formatter:AllureFormatter -o ./output/allure-reports ./features
 
 allure serve ./output/allure-reports --port 9090
+
+PID=$!
+echo PID is $PID
+
+sleep 60
+
+kill $(lsof -t -i:9090)
+
+
